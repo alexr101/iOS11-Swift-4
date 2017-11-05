e0758e1e9f513c4fcf16e6bc540f355dfcac2d43
alexr101 <alex_r_101@hotmail.com>
2017-11-05 16:40:30 -0500

    add IBOutlets and IBActions to view

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +22,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func rollBtnPressed(_ sender: UIButton) {
+        
+        
+    }
+    
 
 
 }
