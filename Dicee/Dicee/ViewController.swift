9a12365fc70087148198def69e4c06af099efb49
alexr101 <alex_r_101@hotmail.com>
2017-11-07 20:31:26 -0500

    removed unneeded folders, and added motion detection to dicee

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -13,12 +13,15 @@ class ViewController: UIViewController {
     var randomDiceNum1 : Int = 0
     var randomDiceNum2 : Int = 0
     
-    @IBOutlet weak var diceImageView1: UIImageView!
-    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var UIDiceImage1: UIImageView!
+    @IBOutlet weak var UIDiceImage2: UIImageView!
+    
+    let diceArr = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6" ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImage()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,13 +30,20 @@ class ViewController: UIViewController {
     }
     
     @IBAction func rollBtnPressed(_ sender: UIButton) {
+        updateDiceImage()
+    }
+    
+    func updateDiceImage(){
         randomDiceNum1 = Int( arc4random_uniform(6) )
         randomDiceNum2 = Int( arc4random_uniform(6) )
         
-        print(randomDiceNum1)
-        
+        UIDiceImage1.image = UIImage(named: diceArr[randomDiceNum1])
+        UIDiceImage2.image = UIImage(named: diceArr[randomDiceNum2])
     }
     
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImage()
+    }
 
 
 }
