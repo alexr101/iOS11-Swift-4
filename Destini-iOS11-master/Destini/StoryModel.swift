839a80c48dd4306dc682d8a4ea13bd612dcf1eef
alexr101 <alex_r_101@hotmail.com>
2017-11-12 20:34:37 -0500

    add story model

---
+++ Destini-iOS11-master/Destini/StoryModel.swift
@@ -0,0 +1,19 @@
+//
+//  StoryModel.swift
+//  Destini
+//
+//  Created by Alex Padilla JR on 11/12/17.
+//  Copyright © 2017 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Story {
+    var optionA: String
+    var optionB: String
+
+    init(option1: String, option2: String) {
+        optionA = option1
+        optionB = option2
+    }
+}
