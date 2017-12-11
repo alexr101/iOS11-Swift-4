188aef2bad6c6c75d577439a0a1be192106aa67e
alexr101 <alex_r_101@hotmail.com>
2017-12-10 23:42:01 -0500

    add equal width to superview and preserver aspect ratio to self(image)

---
+++ Auto Layout Practice/Section11-119/full-width-varied-height/Auto Layout Practice/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Auto Layout Practice
+//
+//  Created by Angela Yu on 01/12/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+//        let mountains = UIImageView(image: UIImage(named: "bw-mountains"))
+//        mountains.frame = CGRect(x:0, y:0, width:self.view.frame.width, height:50)
+//        mountains.contentMode = UIViewContentMode.scaleAspectFit
+//        view.addSubview(mountains)
+//
+//        
+//        mountains.translatesAutoresizingMaskIntoConstraints = false
+//        view.addConstraint(NSLayoutConstraint(item: mountains, attribute: .top, relatedBy: .equal, toItem: self.topLayoutGuide, attribute: .bottom, multiplier: 1, constant: 0))
+//        view.addConstraint(NSLayoutConstraint(item: mountains,
+//                                                        attribute: NSLayoutAttribute.height,
+//                                                        relatedBy: NSLayoutRelation.equal,
+//                                                        toItem: view,
+//                                                        attribute: NSLayoutAttribute.width,
+//                                                        multiplier: view.frame.size.height / view.frame.size.width,
+//                                                        constant: 0))
+
+        
+        
+        
+        
+        
+//        mountainImage.isHidden = true
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
